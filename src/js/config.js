fd22f796f2ad2aa3a3f067dd58206e82740257ad
Avay12 <avaygc@gmail.com>
2023-11-02 09:32:39 +0545

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0d61215c-ac96-4185-b0d1-1d5f73e5b411';
+export const MODAL_CLOSE_SEC = 2.5;
